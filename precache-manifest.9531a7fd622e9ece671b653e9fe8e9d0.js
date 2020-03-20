cd1a56a561fb1cd60447671077e105688956a521
egorsundukou <6940563@gmail.com>
2020-03-19 20:34:35 -0700

    Updates

---
+++ precache-manifest.9531a7fd622e9ece671b653e9fe8e9d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76b0e92f74ca25acabc9be0dcd7f83ca",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "98deadfd213811a7339f",
+    "url": "/counter/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "47f1b1e834bb4a21a2b8",
+    "url": "/counter/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "98deadfd213811a7339f",
+    "url": "/counter/static/js/2.34e8c7bb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/counter/static/js/2.34e8c7bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47f1b1e834bb4a21a2b8",
+    "url": "/counter/static/js/main.33d188a2.chunk.js"
+  },
+  {
+    "revision": "d9e880bebe745aca3830",
+    "url": "/counter/static/js/runtime-main.fc213834.js"
+  }
+]);
